94203556ead5e8fc24cde1ca45fe17cd37ec3b74
rajlanjewar01 <rajl.perficient@gmail.com>
2026-05-05 13:35:25 +0530

    Feature: Redux setup - connect Redux to React

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// 1. import Provider from react/redux library & store
+import { Provider } from 'react-redux';
+import { store } from './store';
 import "./index.css";
 import App from "./App.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* 2. wrap the app component inside the provider */}
+      <Provider store={store}>
+        <App />
+      </Provider>
   </StrictMode>
 );
