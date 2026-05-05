d5503f52ca4a333c53c1b670029fb283e83184a9
rajlanjewar01 <rajl.perficient@gmail.com>
2026-05-05 13:52:54 +0530

    Feature: Form values to update the state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import CarForm from "./components/CarForm";
 
 function App() {
   return (
     <>
+      <CarForm />
     </>
   );
 }
