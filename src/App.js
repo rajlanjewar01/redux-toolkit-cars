6f0c623e30e17a7ae8cae39c374e7e90ff053173
rajlanjewar01 <rajl.perficient@gmail.com>
2026-05-05 16:56:05 +0530

    Optimization: stying

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import CarForm from "./components/CarForm";
 import CarList from "./components/CarList";
+import CarSearch from "./components/CarSearch";
+import CarValue from "./components/CarValue";
 
 function App() {
-  return (
-    <>
-      <CarForm />
-      <CarList />
-    </>
-  );
+	return (
+		<div className="container is-fluid">
+			<CarForm />
+			<CarList />
+			<CarSearch />
+			<CarValue />
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import 'bulma/css/bulma.css';
+import './styles.css';
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // 1. import Provider from react/redux library & store
 import { Provider } from 'react-redux';
 import { store } from './store';
-import "./index.css";
 import App from "./App.js";
 
 createRoot(document.getElementById("root")).render(
