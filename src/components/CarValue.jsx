898e190cf758f7b7291ac0843a2dc6b8f6c1ed4c
rajlanjewar01 <rajl.perficient@gmail.com>
2026-05-07 12:47:13 +0530

    Refactor: total car cost calculation with filter and reduce js functions

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,16 +1,12 @@
 import { useSelector } from 'react-redux';
 
 function CarValue() {
-	const totalCost = useSelector(({ cars: { data, searchTerm }}) => {
-		const filterCars = data.filter((car) => {
-			return car.name?.toLowerCase().includes(searchTerm?.toLowerCase() || '');
-		});
-		let cost = 0;
-		for(let car of filterCars) {
-			cost += car.cost;
-		}
-		return cost;
-	});
+	const totalCost = useSelector(({ cars: { data, searchTerms }}) =>
+		data
+		.filter((car) => car.name?.toLowerCase().includes(searchTerms?.toLowerCase() || '')
+		)
+		.reduce((accumlator, car) => accumlator + car.cost, 0)
+	);
 
 	return (
 		<div className="car-value">
