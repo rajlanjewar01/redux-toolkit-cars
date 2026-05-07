c453b8851bca670e8dac6b97a740c26bc6369e72
rajlanjewar01 <rajl.perficient@gmail.com>
2026-05-07 12:34:12 +0530

    Feature: Get totalcar cost with useSelector

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,7 +1,22 @@
+import { useSelector } from 'react-redux';
+
 function CarValue() {
-    return (
-        <div>car value</div>
-    )
+	const totalCost = useSelector(({ cars: { data, searchTerm }}) => {
+		const filterCars = data.filter((car) => {
+			return car.name?.toLowerCase().includes(searchTerm?.toLowerCase() || '');
+		});
+		let cost = 0;
+		for(let car of filterCars) {
+			cost += car.cost;
+		}
+		return cost;
+	});
+
+	return (
+		<div className="car-value">
+			Totol Cost: $ {totalCost}
+		</div>
+	)
 }
 
 export default CarValue;
